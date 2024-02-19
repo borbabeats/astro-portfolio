c724661cd8233d75106f1068fbd2c01aa54c1030
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-18 23:18:53 -0300

    New portfolio

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,60 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Maicon Borba",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `Sou um Web Developer de Sapucaia do Sul, RS – Brasil. Gosto de estudar e aprender novas tecnologias, principalmente de Front End e Mobile.
+
   `,
-
-  experience: [
-    {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
-      description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
-    },
-  ],
-
-  education: [
-    {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
-    },
-  ],
 
   socialMedia: {
     facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/borbabeats",
+    email: "mailto:maiconbsconceicao@gmail.com",
+    linkedin: "https://www.linkedin.com/in/maicon-borba/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Pneu Dez",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: "/assets/images/pneudez-home.png",
+      githubUrl: "#",
+      liveUrl: "https://pneudez.com.br/",
+    },
+    {
+      title: "Penso",
+      isFeatured: true,
+      thumbnail: "/assets/images/penso-home.png",
+      githubUrl: "#",
+      liveUrl: "https://www.penso.com.br/",
+    },
+    {
+      title: "Gertec Mobile",
+      isFeatured: true,
+      thumbnail: "/assets/images/gertec-home-mobile.png",
+      githubUrl: "#",
+      liveUrl: "https://www.penso.com.br/",
+    },
+    {
+      title: "Gangah - Curso de ritmos",
+      isFeatured: true,
+      thumbnail: "/assets/images/gangah-home.png",
+      githubUrl: "#",
+      liveUrl: "https://gangah.com.br/",
+    },
+    {
+      title: "Federasul - Divisão jovem",
+      isFeatured: true,
+      thumbnail: "/assets/images/federasul-home.png",
+      githubUrl: "#",
+      liveUrl: "https://www.federasul.com.br/divisao-jovem-lp/",
+    },
+    {
+      title: "Manage Landing Page",
+      isFeatured: true,
+      thumbnail: "/assets/images/frontendmentor-manage.png",
+      githubUrl: "https://github.com/borbabeats/manage-landing-page-master",
+      liveUrl: "https://borbabeats.github.io/manage-landing-page-master/",
     },
   ],
 };
